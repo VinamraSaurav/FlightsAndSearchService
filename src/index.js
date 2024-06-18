6f77bc5eec723fced8d8b79654dc11bcae166618
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-06-19 02:04:41 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+
+    const app = express();
+    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended : true }));
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+
+setupAndStartServer();
+
